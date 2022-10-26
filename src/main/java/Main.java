a400637a43a89dfd4574c1d80d6fbecfe74767b7
AndrewAscone <andrew.ascone@gmail.com>
2022-10-26 18:19:54 -0400

    Created a program that takes the user's name as input. It then checks if the name is 'Alice' or 'Bob'. If the user name matches one of those names, it will give the user a greeting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Created by aascone on 10/26/22.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
 
+        //Ask the user to input their name
+        System.out.println("What is your name? ");
+        String name = in.nextLine();
+
+        //A quick test to make sure the input functionality works
+        //System.out.println("Your name is: " + name);
+
+        //Conditional to test if the name is 'Alice' or 'Bob'
+        if (name.equals("Alice") || name.equals("alice") || name.equals("bob") || name.equals("Bob")){
+            System.out.println("Hello " + name + "!");
+        }
+        else{
+            System.out.println("Meh...");
+        }
     }
 }
